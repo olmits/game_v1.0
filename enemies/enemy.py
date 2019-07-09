b888d638e8498ed3f47e57b3a594a390a5ee0e63
Oleksii Mits <oleksii.mits@gmail.com>
2019-07-09 18:21:54 +0300

    Test push

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -37,7 +37,6 @@ class Enemy:
         if X <= self.x + self.width and X >= self.x:
             if Y <= self.y + self.height and Y >= self.y:
                 return True
-
         return False
 
     def move(self):
